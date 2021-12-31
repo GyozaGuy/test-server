20d3e8539d3cb35df3cfe9c5a09359f1d003be4d
GyozaGuy <gyozaguy@gmail.com>
2021-12-30 17:19:39 -0700

    Added ESLint config

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  },
+  extends: ['eslint:recommended'],
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  rules: {
+    indent: ['error', 2, { ignoredNodes: ['TemplateLiteral *'], SwitchCase: 1 }],
+    'linebreak-style': ['error', 'unix'],
+    'max-len': ['warn', { code: 100 }],
+    'no-unused-vars': ['warn'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always']
+  }
+};

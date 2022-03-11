093a938b2de38fb2ba41d64485a387fdf823bbc6
GyozaGuy <gyozaguy@gmail.com>
2022-03-11 15:37:38 -0700

    Component work

--- src/index.js
+++ src/index.js
@@ -2,3 +2,32 @@ import 'nwc-library';
 import 'regenerator-runtime/runtime';
 
 import './index.css';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const openDialogButton = document.body.querySelector('#openDialogButton');
+  const dialog = document.body.querySelector('#dialog');
+
+  for (const closeDialogButton of [...dialog.querySelectorAll('.closeDialogButton')]) {
+    closeDialogButton.addEventListener('click', () => {
+      dialog.close();
+    });
+  }
+
+  openDialogButton.addEventListener('click', () => {
+    dialog.showModal();
+  });
+
+  dialog.addEventListener('close', ({ target }) => {
+    target.removeAttribute('opened');
+  });
+
+  const dialogObserver = new MutationObserver(mutations => {
+    for (const mutation of mutations) {
+      if (mutation.type === 'attributes' && mutation.attributeName === 'open') {
+        mutation.target.setAttribute('opened', '');
+      }
+    }
+  });
+
+  dialogObserver.observe(dialog, { attributes: true });
+});

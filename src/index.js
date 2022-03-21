73806f26bac07c5a7705c9842f50446f49d0fd56
GyozaGuy <gyozaguy@gmail.com>
2022-03-20 22:54:00 -0600

    Replaced dialog with ui-dialog

--- src/index.js
+++ src/index.js
@@ -6,29 +6,14 @@ import './index.css';
 
 document.addEventListener('DOMContentLoaded', () => {
   const openDialogButton = document.body.querySelector('#openDialogButton');
-  const dialog = document.body.querySelector('#dialog');
-
-  for (const closeDialogButton of [...dialog.querySelectorAll('.closeDialogButton')]) {
-    closeDialogButton.addEventListener('click', () => {
-      dialog.close();
-    });
-  }
+  const dialog = document.body.querySelector('ui-dialog');
+  const closeDialogButton = document.body.querySelector('#closeDialogButton');
 
   openDialogButton.addEventListener('click', () => {
     dialog.showModal();
   });
 
-  dialog.addEventListener('close', ({ target }) => {
-    target.removeAttribute('opened');
+  closeDialogButton.addEventListener('click', () => {
+    dialog.close();
   });
-
-  const dialogObserver = new MutationObserver(mutations => {
-    for (const mutation of mutations) {
-      if (mutation.type === 'attributes' && mutation.attributeName === 'open') {
-        mutation.target.setAttribute('opened', '');
-      }
-    }
-  });
-
-  dialogObserver.observe(dialog, { attributes: true });
 });

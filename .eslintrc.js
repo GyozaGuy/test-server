c29c2b94d280f2d47c8a8faeba287f1fd8779c53
GyozaGuy <gyozaguy@gmail.com>
2022-05-06 18:38:36 -0600

    Updated ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     es6: true,
     node: true
   },
-  extends: ['eslint:recommended', 'plugin:prettier/recommended', 'prettier'],
+  extends: ['eslint:recommended'],
   parser: '@babel/eslint-parser',
   parserOptions: {
     ecmaVersion: 12,
@@ -13,6 +13,10 @@ module.exports = {
   rules: {
     indent: ['error', 2, { ignoredNodes: ['TemplateLiteral *'], SwitchCase: 1 }],
     'linebreak-style': ['error', 'unix'],
-    'no-unused-vars': 'warn'
+    'max-len': ['warn', { code: 100 }],
+    'no-unused-vars': 'warn',
+    'object-curly-spacing': ['error', 'always'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always']
   }
 };

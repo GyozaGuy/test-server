7b2280dc2aa59b5ed6a2f0721b902b63f2843ebc
GyozaGuy <gyozaguy@gmail.com>
2022-05-07 16:17:52 -0600

    Call working

--- src/index.js
+++ src/index.js
@@ -1,4 +1,35 @@
 import 'nwc-library';
+import Peer from 'peerjs';
 import 'regenerator-runtime/runtime';
 
 import './index.css';
+
+const peer = new Peer();
+
+peer.on('open', id => {
+  const peerId = document.body.querySelector('#peerId');
+  const destPeerId = document.body.querySelector('#destPeerId');
+  const callButton = document.body.querySelector('#callButton');
+  const contactVideo = document.body.querySelector('#contactVideo');
+
+  peerId.textContent = id;
+
+  callButton.addEventListener('click', async () => {
+    const call = peer.call(
+      destPeerId.value,
+      await navigator.mediaDevices.getUserMedia({ audio: true, video: true })
+    );
+
+    call.on('stream', stream => {
+      contactVideo.srcObject = stream;
+    });
+  });
+
+  peer.on('call', async call => {
+    call.answer(await navigator.mediaDevices.getUserMedia({ audio: true, video: true }));
+
+    call.on('stream', stream => {
+      contactVideo.srcObject = stream;
+    });
+  });
+});

2402c7ad0e0e7eefbf9e018fff85d312c7627bd0
GyozaGuy <gyozaguy@gmail.com>
2022-08-03 16:03:06 -0600

    Revert "Basic config and chart generation working (no lines)"
    This reverts commit 3d5d06c4febf2f084cab5a2e45dcdb76d154e291.

--- src/index.js
+++ src/index.js
@@ -1,40 +1,4 @@
 import 'nwc-library';
 import 'regenerator-runtime/runtime';
 
-import dragonConfig from './configs/dragon';
-import { html, select } from './helpers/dom';
 import './index.css';
-
-document.addEventListener('DOMContentLoaded', () => {
-  applyConfig(dragonConfig);
-});
-
-function applyConfig(config) {
-  const canvas = select('#canvas');
-  canvas.style.backgroundImage = `url("${config.backgroundImage}")`;
-
-  const fragment = document.createDocumentFragment();
-
-  renderPerson(config.person, fragment);
-  canvas.appendChild(fragment);
-}
-
-function renderPerson(person, fragment) {
-  const [left, bottom] = person.location;
-
-  fragment.appendChild(html`
-    <div
-      class="person"
-      style="
-        bottom: ${bottom}%;
-        left: ${left}%;
-      "
-    >
-      ${person.name}
-    </div>
-  `);
-
-  for (const parent of person.parents) {
-    renderPerson(parent, fragment);
-  }
-}
